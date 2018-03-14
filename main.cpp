16ecd46c4855b2e62de98400c83d0fa7daa72d86
Rajan Patel <rajan.n.patel.97@gmail.com>
2018-03-14 22:52:53 +0000

    yeezy season3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "mbed.h"
-#include "Crypto_light/hash/SHA256.h"
-#include "mbed-rtos/rtos/rtos.h"
+#include "SHA256.h"
+#include "rtos.h"
+
+#define char_len_max 32
 
 //Photointerrupter input pins
 #define I1pin D2
@@ -9,7 +11,7 @@
 
 //Incremental encoder input pins
 #define CHA   D7
-#define CHB   D8  
+#define CHB   D8
 
 //Motor Drive output pins   //Mask in output byte
 #define L1Lpin D4           //0x01
@@ -18,14 +20,6 @@
 #define L2Hpin D6           //0x08
 #define L3Lpin D9           //0x10
 #define L3Hpin D10          //0x20
-
-//Enum for putMessage message types
-#define MSG_HASHCOUNT 0
-#define MSG_NONCE_OK 1
-#define MSG_OVERFLOW 2
-
-//FIFO constant definitions
-#define MAX_ARRAY_SIZE 256 //check the variable fifo position if this is changed
 
 //Mapping from sequential drive states to motor phase outputs
 /*
@@ -39,18 +33,18 @@ State   L1  L2  L3
 6       -   -   -
 7       -   -   -
 */
-
 //Drive state to output table
 const int8_t driveTable[] = {0x12,0x18,0x09,0x21,0x24,0x06,0x00,0x00};
 
 //Mapping from interrupter inputs to sequential rotor states. 0x00 and 0x07 are not valid
-const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};  
+const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};
 //const int8_t stateMap[] = {0x07,0x01,0x03,0x02,0x05,0x00,0x04,0x07}; //Alternative if phase order of input or drive is reversed
 
 //Phase lead to make motor spin
-const int8_t lead = -2;  //2 for forwards, -2 for backwards
+const int8_t lead = 2;  //2 for forwards, -2 for backwards
 
 //Status LED
+
 DigitalOut led1(LED1);
 
 //Photointerrupter inputs
@@ -59,218 +53,269 @@ InterruptIn I2(I2pin);
 InterruptIn I3(I3pin);
 
 //Motor Drive outputs
-DigitalOut L1L(L1Lpin);
+PwmOut L1L(L1Lpin);
 DigitalOut L1H(L1Hpin);
-DigitalOut L2L(L2Lpin);
+PwmOut L2L(L2Lpin);
 DigitalOut L2H(L2Hpin);
-DigitalOut L3L(L3Lpin);
+PwmOut L3L(L3Lpin);
 DigitalOut L3H(L3Hpin);
 
-RawSerial pc(SERIAL_TX, SERIAL_RX); 
- 
-//structure for Mail Class
-typedef struct {
-     uint8_t code;
-     uint32_t data;
-} message_t ;
- 
-//Mail class allowing 16 messages to be stored up in the FIFO
-Mail<message_t,16> outMessages;
+//Initialise the serial port
+RawSerial pc(SERIAL_TX, SERIAL_RX);
 
+//***********Initialisation Our Variables************//
 
-void putMessage(uint8_t code, uint32_t data)
-{
-    message_t *pMessage = outMessages.alloc();
+//Message IDs
+enum message_code {
+    ERROR_C = 0, //Error message ID
+    HASH = 1, //Hash frequency ID
+    NONCE = 2, //correct nonce ID
+    POSITION = 3, //Starting Rotor ID
+    DECODED = 4 //Decoded message ID
+};
+
+//message structure
+typedef struct{
+    uint8_t code; //ID
+    uint32_t data; //Data
+    } message_t;
+
+Mail<message_t,16> outMessages; //Output message queue
+Queue<void, 8> inCharQ; //character inputs
+
+int8_t orState; //starting state of the rotor
+
+uint8_t sequence[] = {0x45,0x6D,0x62,0x65,0x64,0x64,0x65,0x64,0x20,0x53,0x79,0x73,0x74,0x65,0x6D,0x73,0x20,0x61,0x72,0x65,0x20,0x66,0x75,0x6E,0x20,0x61,0x6E,0x64,0x20,0x64,0x6F,0x20,0x61,0x77,0x65,0x73,0x6F,0x6D,0x65,0x20,0x74,0x68,0x69,0x6E,0x67,0x73,0x21,0x20,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+uint64_t* key = (uint64_t*)((int)sequence + 48); //Key generation
+uint64_t* nonce = (uint64_t*)((int)sequence + 56); //Nonce
+uint8_t hash[32]; //Hash output
+
+char commInChar[char_len_max]; //array 32 characters length
+uint8_t ptr; //char array pointer
+
+volatile uint64_t newKey; //means value can change between thread calls
+Mutex newKey_mutex; //Stops the value from beng changed during use
+
+Thread commOutT; //Output Thread
+Thread commInT; //Input Thread
+
+int pwm_period = 256;
+uint32_t m_torque = pwm_period;
+
+void putMessage(uint8_t code, uint32_t data){
+    message_t *pMessage = outMessages.alloc(); //allocated the recieved message to  outmessages
     pMessage->code = code;
     pMessage->data = data;
     outMessages.put(pMessage);
-}
+    }
 
-Thread commOutT;
- 
-void commOutFn()
-{  
-    while(1) {
-        osEvent newEvent = outMessages.get();
-        message_t *pMessage = (message_t*)newEvent.value.p;
-        pc.printf("Message %d with data 0x%016x\r\n",
-                    pMessage->code,pMessage->data);
-        outMessages.free(pMessage);
-     }
-}
-//Global varible for the Bitcoin Key
-volatile uint64_t newKey = 0; //check initialise value? ****
- 
-Mutex newKey_mutex;//for mutex locking 
+void commOutFn(){
+    while(1){
+        osEvent newEvent = outMessages.get(); //pulls the message
+        message_t *pMessage = (message_t*)newEvent.value.p; //assigns the values to pmessage
 
-//Queue class
-Queue<void, 8> inCharQ;
-//serial port ISR to take individual chars
-void serialISR(){
- uint8_t newChar = pc.getc();
- inCharQ.put((void*)newChar);
-}
- 
- //decode commands
-Thread decodeT;
-
-//set the global NewKey
-void setNewCmd(char newCmd[MAX_ARRAY_SIZE]){
-     
-    //K 
-    if (newCmd[0] == 'K'){
-        newKey_mutex.lock();
-        sscanf(newCmd, "K%x", &newKey); //Decode the command
-        newKey_mutex.unlock();
+        switch(pMessage->code){ //finds correct ID for message
+            case ERROR_C:
+                if(pMessage->data == 0){ //Input message was too large
+                    pc.printf("Input command too large\n\r");
+                }
+                break;
+            case HASH:
+                pc.printf("Hash frequency %d Hz \n\r",pMessage->data); //outputs the hash frequency
+                break;
+            case NONCE:
+                pc.printf("Found a nonce 0x%016x\n\r", pMessage->data); //outputs correct nonce
+                break;
+            case POSITION:
+                pc.printf("Rotor Starting Position: %d\n\r", pMessage->data); //outputs starting position
+                break;
+            case DECODED:
+                if (pMessage->data == 0) {
+                    pc.printf("Decoded as R\n\r");
+                }
+                else if (pMessage->data == 1) {
+                    pc.printf("Decoded max_speed S\n\r");
+                }
+                else if (pMessage->data == 2) {
+                    newKey_mutex.lock(); //outputs the key input
+                    pc.printf("Decoded New Key 0x%x\n\r",(uint64_t)newKey);
+                    newKey_mutex.unlock();
+                }
+                break;
+        }
+        outMessages.free(pMessage); //removes the message
     }
 }
 
+void serialISR(){
+    uint8_t newChar = pc.getc(); //gets valuee from serial port
+    pc.putc(newChar);
+    inCharQ.put((void*)newChar); //places into newChar
+    }
 
-//decode char's function
-void decodeFn(){
-    pc.attach(&serialISR);
-    char newCmd[MAX_ARRAY_SIZE] = "";
-    uint32_t bufferPosition = 0;                //change this variable type if the max buffer/fifio size is found to be different
-    bool exit = false;
-    while(!exit) { 
-    
-        //get new char
-        osEvent newEvent = inCharQ.get();
+void decode_char(char* buffer, uint8_t index){
+
+    if(buffer[index] == 'R'){ //if first value is R rotate cretain number of times
+        putMessage(DECODED,0);
+
+    }
+    else if(buffer[index] == 'r'){ //if first value is R rotate cretain number of times
+        putMessage(DECODED,0);
+
+    }
+    else if(buffer[index] == 'V'){ //if first value is V set speed of rotation
+        putMessage(DECODED,1);
+    }
+    else if(buffer[index] == 'v'){ //if first value is V set speed of rotation
+        putMessage(DECODED,1);
+    }
+    else if (buffer[index] == 'K'){ //if char is K set key to value input
+        newKey_mutex.lock();
+        sscanf(buffer, "K%10llx", &newKey);
+        newKey_mutex.unlock();
+        putMessage(DECODED,2);
+    }
+    else if (buffer[index] == 'k'){ //if char is K set key to value input
+        newKey_mutex.lock();
+        sscanf(buffer, "k%10llx", &newKey);
+        newKey_mutex.unlock();
+        putMessage(DECODED,2);
+    }
+    else if (buffer[index] == 'T') { //torque test
+        putMessage(DECODED, 4);
+        sscanf(buffer, "T%x", &newKey); //Decode the command and put it to newkey
+        putMessage(DECODED, newKey);
+        m_torque = newKey;
+    }
+    else if (buffer[index] == 't') { //torque test
+        putMessage(DECODED, 4);
+        sscanf(buffer, "T%x", &newKey); //Decode the command and put it to newkey
+        putMessage(DECODED, newKey);
+        m_torque = newKey;
+    }
+
+}
+
+void commInFn(){
+    pc.printf("Enter your command:\n\r"); //Tells the person to input their message
+    pc.attach(&serialISR); //looks for the serialISR to get message
+    while(1){
+        if(ptr >= char_len_max){
+            putMessage(ERROR_C,0); //if gone over the buffer length, cancel and restart for next input
+            ptr = 0; //reset pointer
+            break;
+        }
+        osEvent newEvent = inCharQ.get(); //get next character
         uint8_t newChar = (uint8_t)newEvent.value.p;
-        
-        //check for carriage return "\r"
-        if(newChar == 'r'){
-            if(bufferPosition != 0){
-                if(newCmd[bufferPosition - 1] == '\\'){
-                    //carriage found 
-                    newChar = '0';  //replace character
-                    
-                    //add to array
-                    newCmd[bufferPosition] = newChar; 
-                    
-                    //reset buffer
-                    bufferPosition = 0;
-                    //send char array to decoder ***
-                    setNewCmd(newCmd);
-                }
-            }
+        if(newChar != '\r' && newChar != '\n'){
+            commInChar[ptr] = newChar; //place values into buffer
+            ptr++; //increment pointer
         }
-        //Add new char to array
         else{
-            //add character at current position 
-            newCmd[bufferPosition] = newChar; 
-            bufferPosition ++;
+            commInChar[ptr] = '\0'; //defines the end of the command
+            ptr = 0; //resets the pointer
+            decode_char(commInChar,ptr); //sends array to decoding function
         }
-      
-        //------error for overflow-------------------------// 
-        if(bufferPosition >= MAX_ARRAY_SIZE ){ 
-            exit = true; 
-            putMessage(MSG_OVERFLOW, bufferPosition); //
-        } 
-        //-------------------------------------------------//
-      
-     }//end of : while(!exit){} 
-        
-        //iii. Test the first character to determine which command was sent.
-        //iv. Decode the rest of the command
-} 
+    }
+}
 
 //Set a given drive state
-void motorOut(int8_t driveState){
-    
+void motorOut(int8_t driveState, uint32_t t){
     //Lookup the output byte from the drive state.
     int8_t driveOut = driveTable[driveState & 0x07];
       
     //Turn off first
-    if (~driveOut & 0x01) L1L = 0;
+    if (~driveOut & 0x01) L1L.pulsewidth_us(0);
     if (~driveOut & 0x02) L1H = 1;
-    if (~driveOut & 0x04) L2L = 0;
+    if (~driveOut & 0x04) L2L.pulsewidth_us(0);
     if (~driveOut & 0x08) L2H = 1;
-    if (~driveOut & 0x10) L3L = 0;
+    if (~driveOut & 0x10) L3L.pulsewidth_us(0);
     if (~driveOut & 0x20) L3H = 1;
     
     //Then turn on
-    if (driveOut & 0x01) L1L = 1;
+    if (driveOut & 0x01) L1L.pulsewidth_us(m_torque);
     if (driveOut & 0x02) L1H = 0;
-    if (driveOut & 0x04) L2L = 1;
+    if (driveOut & 0x04) L2L.pulsewidth_us(m_torque);
     if (driveOut & 0x08) L2H = 0;
-    if (driveOut & 0x10) L3L = 1;
+    if (driveOut & 0x10) L3L.pulsewidth_us(m_torque);
     if (driveOut & 0x20) L3H = 0;
     }
-    
-volatile uint16_t hashcount = 0;
- 
-void do_hashcount() 
-{
-    putMessage(MSG_HASHCOUNT, hashcount);
-    hashcount = 0;
-}
-    
-    //Convert photointerrupter inputs to a rotor state
+
+//Convert photointerrupter inputs to a rotor state
 inline int8_t readRotorState(){
     return stateMap[I1 + 2*I2 + 4*I3];
-    }
+}
 
-//Basic synchronisation routine    
+//Basic synchronisation routine
 int8_t motorHome() {
     //Put the motor in drive state 0 and wait for it to stabilise
-    motorOut(0);
-    wait(1.0);
+    motorOut(0, pwm_period);
+    wait(2.0);
     
     //Get the rotor state
     return readRotorState();
 }
 
-int8_t orState;
-int8_t intState;
-
-void spinmotorISR() {
-    intState = readRotorState();
-    //Run the motor synchronisation
-    motorOut((intState-orState+lead+6)%6); //+6 to make sure the remainder is positive
+int32_t motorPosition;
+void ISR(){
+    static int8_t oldRotorState;
+    int8_t rotorState = readRotorState(); //reads motor position
+    motorOut((rotorState-orState+lead+6)%6, m_torque); //+6 to make sure the remainder is positive
+    if (rotorState - oldRotorState == 5) motorPosition--;
+    else if (rotorState - oldRotorState == -5) motorPosition++;
+    else motorPosition += (rotorState - oldRotorState);
+    oldRotorState = rotorState;
 }
 
+volatile uint16_t hashcount = 0;
+
+void do_hashcount() 
+{
+    putMessage(HASH, hashcount);
+    hashcount = 0;
+}
+
+void init() {
+    L1L.period_us(pwm_period);
+    L2L.period_us(pwm_period);
+    L3L.period_us(pwm_period);
+}
 
 //Main
-int main() {
-    orState = motorHome();
-    pc.printf("Rotor origin: %x\n\r",orState);
-    
+int main(){
+    pc.printf("Hello\n\r"); //outputs hello when turned on
+    init();
+    commOutT.start(commOutFn); //starts the output and input threads
+    commInT.start(commInFn);
+    //Run the motor synchronisation
+    orState = motorHome(); //finds staring position
+    putMessage(POSITION,orState);
+
     //orState is subtracted from future rotor state inputs to align rotor and motor states
-    I1.rise(&spinmotorISR);
-    I1.fall(&spinmotorISR);
-    I2.rise(&spinmotorISR);
-    I2.fall(&spinmotorISR);
-    I3.rise(&spinmotorISR);
-    I3.fall(&spinmotorISR);
-    spinmotorISR();
+    //Poll the rotor state and set the motor outputs accordingly to spin the motor
+    I1.rise(&ISR); //looks for rising edge to trigger the motor change
+    I2.rise(&ISR);
+    I3.rise(&ISR);
+    I1.fall(&ISR); //looks for rising edge to trigger the motor change
+    I2.fall(&ISR);
+    I3.fall(&ISR);
+    ISR(); //runs the ISR to begin with to start the motor running
     
     Ticker hashcounter;
     hashcounter.attach(&do_hashcount, 1.0);
-    
-    commOutT.start(&commOutFn);
-    decodeT.start(&decodeFn);
-    
-    uint8_t sequence[] = {0x45,0x6D,0x62,0x65,0x64,0x64,0x65,0x64,
-                            0x20,0x53,0x79,0x73,0x74,0x65,0x6D,0x73,
-                            0x20,0x61,0x72,0x65,0x20,0x66,0x75,0x6E,
-                            0x20,0x61,0x6E,0x64,0x20,0x64,0x6F,0x20,
-                            0x61,0x77,0x65,0x73,0x6F,0x6D,0x65,0x20,
-                            0x74,0x68,0x69,0x6E,0x67,0x73,0x21,0x20,
-                            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-                            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-    
-    uint64_t* key = (uint64_t*)((int)sequence + 48);
-    uint64_t* nonce = (uint64_t*)((int)sequence + 56);
-    uint8_t hash[32];
-    
+
     while (1) {
-        SHA256::computeHash(hash, sequence, 64);
-        
-        if (hash[0] == 0 && hash[1] == 0) {
-            putMessage(MSG_NONCE_OK, *nonce);
+        newKey_mutex.lock(); //stops value from being changed
+        *key = newKey; //changed key
+        newKey_mutex.unlock(); //releases the value
+
+        SHA256::computeHash(&hash[0],&sequence[0],sizeof(sequence)); //computes the hash
+       
+        if((hash[0] == 0) && (hash[1] == 0)){
+            putMessage(NONCE,*nonce); //when hash is correct print the nonce
         }
-        (*nonce)++;
+
+        *nonce += 1; //increments nonce
         hashcount++;
     }
 }
